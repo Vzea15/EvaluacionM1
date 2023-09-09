d714bbad48c6131c2b394c6e0d3d74ee01221188
valentina <amarileszea15@gmail.com>
2023-09-09 15:48:45 -0500

    c1

---
+++ actividad.py
@@ -0,0 +1,24 @@
+
+
+def calcular(notas_total):
+    total_notas = sum(notas_total)
+    promedio = total_notas / len(notas_total)
+    promedio
+    return 
+
+for estudiante, notas in notas_estudiantes.items():
+    promedio = calcular(notas)
+   
+
+notas_estudiantes = {
+    'valentina': [3.0, 4.2, 1.9],
+    'juan': [3.5, 1.5, 4.5],
+    'manuela': [5.0, 4.6, 2.6]
+}
+
+print ("las notas de los estudiantes", promedio)
+
+
+
+
+
